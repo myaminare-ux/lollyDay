dcf3d7a1898d6f87e18bc98cdd8384eb2cc3b19f
myaminare-ux <myaminare@gmail.com>
2026-09-07 18:09:11 +0900

    5행 제작중

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 /* index.js */
+// 베스트
 const best_swiper = document.querySelector('.best_item_list');
 
 for (let i = 0; i<3; i++) {
     // ----------------------------------------------- 클래스 생성
     const best_slide = document.createElement('div');
     // ----------------------------------------------- 클래스 연결
-    best_slide.classList.add('swiper-slide');
+    best_slide.classList.add('swiper-slide', 'best_slide');
     // ----------------------------------------------- HTML 연결
     best_slide.innerHTML = `
     <a href="#" target="_blank" class="best_item">
@@ -17,19 +18,87 @@ for (let i = 0; i<3; i++) {
             <h3>${bestDB[i].title}</h3>
             <del class="item_origin_price">${bestDB[i].originPrice.toLocaleString('ko-KR')}원</del>
             <div class="sale_price">
-                <p class="sale">${bestDB[i].discount}%</p>
+                <p class="sale">${bestDB[i].discount}</p>
                 <p class="price">${bestDB[i].salePrice.toLocaleString('ko-KR')}원</p>
             </div>
         </div>
     </a>
     <button type="button" class="favorite_btn">
-        <img src="./images/favorite_items_icon.png" alt="관심상품 등록">
+        <img src="./images/favorite_items_icon2.png" alt="관심상품 등록">
     </button>`;
+
+    // 관심상품 등록 버튼 클릭 시 활성화 이미지 변경
+    const favBtn = best_slide.querySelector('.favorite_btn');
+    const favImg = best_slide.querySelector('.favorite_btn > img');
+    
+    favBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+        favBtn.classList.toggle('active');
+        if(favBtn.classList.contains('active')){
+            favImg.src = './images/favorite_items_active_icon.png';
+        } else {
+            favImg.src = './images/favorite_items_icon2.png';
+        }
+    });
+
     best_swiper.children[0].appendChild(best_slide);
 }
+
 
 const best_swiper_func = new Swiper(best_swiper, {
     slidesPerView:3,
     spaceBetween:20,
-    observer: true,
+    observer:true,
+    watchOverflow:false,
+    pagination:{
+        el:'.best_item_list .swiper-pagination',
+        //type:'bullets'(기본)
+        //type:'fraction' (숫자)
+        //type:'progressbar' (바)
+        type:'fraction',
+    },
+    navigation:{
+        nextEl:'.best_item_list .swiper-button-next',
+        prevEl:'.best_item_list .swiper-button-prev',
+    },
 });
+
+// 신상품
+const newUl = document.querySelector('.new_item_list');
+
+for(let i=0; i<4; i++){
+    const newLi = document.createElement('li');
+    newLi.innerHTML = `
+    <a href="#" target="_blank" class="new_item">
+        <div class="item_top">
+            <img src="${newDB[i].img}" alt="${newDB[i].title}">
+        </div>
+        <div class="item_btm">
+            <h3>${newDB[i].title}</h3>
+                <del class="item_origin_price">${newDB[i].originPrice.toLocaleString('ko-KR')}원</del>
+                <div class="sale_price">
+                    <p class="sale">${newDB[i].discount}</p>
+                    <p class="price">${newDB[i].salePrice.toLocaleString('ko-KR')}원</p>
+                </div>
+        </div>
+    </a>
+    <button type="button" class="favorite_btn">
+        <img src="./images/favorite_items_icon2.png" alt="관심상품 등록">
+    </button>`;
+
+    // 관심상품 등록 버튼 클릭 시 활성화 이미지 변경
+    const favBtn = newLi.querySelector('.favorite_btn');
+    const favImg = newLi.querySelector('.favorite_btn > img');
+    
+    favBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+        favBtn.classList.toggle('active');
+        if(favBtn.classList.contains('active')){
+            favImg.src = './images/favorite_items_active_icon.png';
+        } else {
+            favImg.src = './images/favorite_items_icon2.png';
+        }
+    });
+
+    newUl.appendChild(newLi);
+}
---
+++ scripts/items_db.js
@@ -0,0 +1,67 @@
+/* best_db.js */
+// 베스트
+const bestDB = [
+{
+    id:1,
+    title:'내 A6 노트를 보호해 줄 커버',
+    size:'A6',
+    img:'./images/best_item1.jpg',
+    originPrice:'16,000',
+    discount:'5%',
+    salePrice:'15,200',
+},
+{
+    id:2,
+    title:'A6 노트 커버와 함께 사용하는 스트랩',
+    size:'A6',
+    img:'./images/best_item2.jpg',
+    originPrice:'1,300',
+    discount:'5%',
+    salePrice:'1,235',
+},
+{
+    id:3,
+    title:'기록 스타일에 맞게 골라 쓰는 A6 노트 5종',
+    size:'A6',
+    img:'./images/best_item3.jpg',
+    originPrice:'11,000',
+    discount:'5%',
+    salePrice:'10,450',
+},
+];
+
+const newDB = [
+{
+    id:1,
+    title:'집 안 기분 좋은 포인트! 못나니즈 사각 쿠션',
+    img:'./images/new_item1.jpg',
+    originPrice:'43,000',
+    discount:'15%',
+    salePrice:'36,550',
+},
+{
+    id:2,
+    title:'티켓, 사진을 쏙! 나만의 작은 기억 도서관',
+    img:'./images/new_item2.jpg',
+    originPrice:'32,000',
+    discount:'50%',
+    salePrice:'16,000',
+},
+{
+    id:1,
+    title:'티켓, 사진을 쏙! 나만의 작은 기억 도서관',
+    img:'./images/new_item3.jpg',
+    originPrice:'9,000',
+    discount:'5%',
+    salePrice:'8,550',
+},
+{
+    id:1,
+    title:'생활방수 가능한 TPL 스트링 펜 케이스',
+    img:'./images/new_item4.jpg',
+    originPrice:'14,500',
+    discount:'5%',
+    salePrice:'13,770',
+},
+
+];
